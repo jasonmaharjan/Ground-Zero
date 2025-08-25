ab7f7902071d500fccddcc158b9e7e462ab6b478
Jason <jason8maharjan@gmail.com>
2025-08-25 08:16:10 +0545

    refactor: Updated radio component

--- src/utils/bgMusic.ts
+++ src/utils/bgMusic.ts
@@ -9,30 +9,27 @@ import bgMusic8 from "@/assets/sounds/bg_music_8.mp3";
 import bgMusic9 from "@/assets/sounds/bg_music_9.mp3";
 import bgMusic10 from "@/assets/sounds/bg_music_10.mp3";
 
+const tracks = [
+    { musicSrc: bgMusic1, message: "Go to Hill and Go Down, it creates such good luck, and you'll smell bad" },
+    { musicSrc: bgMusic2, message: "Chase your dreams, do not chase your goals, much easier!" },
+    { musicSrc: bgMusic3, message: "The marvelous thing about a joke with a double meaning is that it can only mean 1 thing." },
+    { musicSrc: bgMusic4, message: "Look at this photograph, graph, gra, gr, gre?? When's the date? When are we doing it? ;-;" },
+    {
+        musicSrc: bgMusic5,
+        message: "To look beyond the observable universe, Stand on the head of giants, not on their shoulders. You'll see even more.",
+    },
+    { musicSrc: bgMusic6, message: "Sike! That's the right number! It was the right number all along...Now call her, you dimwit..." },
+    { musicSrc: bgMusic7, message: "My warframe is strong. You are not. JK. My warframe and you both are strong (not). It is a weak world." },
+    {
+        musicSrc: bgMusic8,
+        message: "Time is short they said...so just...let your dreams be dreams. Felt good reading that? Ediot, go chase it now.",
+    },
+    { musicSrc: bgMusic9, message: "Watahh she waahh kitabb!?? Proceeds to hurl a book at you and yell 'Padhaaaa!'" },
+    { musicSrc: bgMusic10, message: "Ah... Nostalgia.. The good ole days.. Now look at you, what even happened to you? Why did you grow up?" },
+    { musicSrc: bgMusic1, message: "If you are here, stay here, its cozy and comfy :-)" },
+];
+
 export const randomizeRadio = () => {
     const randomIndex = Math.floor(Math.random() * 10) + 1;
-    switch (randomIndex) {
-        case 0:
-            return { musicSrc: bgMusic1, message: "Go to Hill and Go Down, it's good luck :-)" };
-        case 1:
-            return { musicSrc: bgMusic2, message: "Chase your dreams, do not chase your goals!" };
-        case 2:
-            return { musicSrc: bgMusic3, message: "The marvelous thing about a joke with a double meaning is that it can only mean 1 thing." };
-        case 3:
-            return { musicSrc: bgMusic4, message: "Look at this photograph, graph, gra, gr, gre?? When's the date ;-;" };
-        case 4:
-            return { musicSrc: bgMusic5, message: "To look beyond the observable universe, Stand on the head of giants, not on their shoulders" };
-        case 5:
-            return { musicSrc: bgMusic6, message: "Sike! That's the right number! It was all along...Now go call her, you dimwit..." };
-        case 6:
-            return { musicSrc: bgMusic7, message: "My warframe is strong. You are not. JK. My warframe and you both are strong (not)" };
-        case 7:
-            return { musicSrc: bgMusic8, message: "Ohnoo Time is short...so just...let your dreams be dreams. Ez :-)" };
-        case 8:
-            return { musicSrc: bgMusic9, message: "Watahh she waahh kitabb!?? Proceeds to hurl a book at you" };
-        case 9:
-            return { musicSrc: bgMusic10, message: "Ah... Nostalgia.. The good ole days.. Now look at you, what even happened to you? Why did you grow up?" };
-        default:
-            return { musicSrc: bgMusic1, message: "Go to Hill and Go Down, it's good luck :-)" };
-    }
+    return tracks[randomIndex];
 };
